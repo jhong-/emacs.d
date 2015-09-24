bd7eb79eb3124cad9c68442376107cdbb89b9c28
Jim Hong <jimqhong@gmail.com>
2015-09-24 00:29:52 -0700

    Added some basic packages

--- init.el
+++ init.el
@@ -4,3 +4,33 @@
                          ("melpa-stable" . "http://stable.melpa.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")))
 (package-initialize)
+
+; Not sure how this exactly works yet
+(defun require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION. If NO-REFRESH is
+  non-nil, the available package lists will not be called and re-downloaded in
+  order to locate PACKAGE."
+  (if (package-installed-p package min-version)
+    t
+    (if (or (assoc package package-archive-contents) no-refresh)
+      (package-install package)
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
+
+; Setup theme
+(require-package 'zenburn-theme)
+(load-theme 'zenburn t)
+
+(require-package 'better-defaults)
+(require-package 'helm)
+(require-package 'magit)
+(require-package 'projectile)
+
+; Clojure Development
+(require-package 'cider)
+(require-package 'clojure-mode)
+
+; Enable evil-mode
+(require-package 'evil)
+(evil-mode 1)
