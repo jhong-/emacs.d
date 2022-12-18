fbf92c9b26c38f95a018dc3ec1859c4614017417
Jim Hong <jimqhong@gmail.com>
2022-12-18 10:17:32 -0800

    Updated package management for latest emacs

--- init.el
+++ init.el
@@ -1,18 +1,16 @@
 ; Setting up the package manager
 (require 'package)
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("melpa-stable" . "http://stable.melpa.org/packages/")
-                         ("melpa" . "http://melpa.org/packages/")))
+(add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-(unless package-archive-contents
-  (package-refresh-contents))
-
-(setq use-package-always-ensure t
-      use-package-verbose t)
-
 (unless (package-installed-p 'use-package)
+  (package-refresh-contents)
   (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t
+        use-package-expand-minimally t))
 
 (use-package exec-path-from-shell
   :if (memq window-system '(mac ns))
